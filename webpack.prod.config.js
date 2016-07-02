42a0dd7f3db9f9873eaee3e392870bfbb5af95fe
ali404 <acerrahoglu.ali@gmail.com>
2016-07-02 19:24:35 +0300

    ready for prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,17 +1,64 @@
 const path = require('path')
 const webpack = require('webpack')
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const PATHS = {
+    app: path.join(__dirname, 'src/entry.js'),
+    js: path.join(__dirname, 'src/js'),
+    css: path.join(__dirname, 'src/css'),
+    build: path.join(__dirname, 'public/build')
+}
+
 module.exports = {
     devtool: 'source-map',
-    entry: [
-        './src/entry'
-    ],
+    entry: {
+        app: PATHS.app
+    },
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-        publicPath: '/public'
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    resolve: ['', '.js', '.jsx'],
+    module: {
+        loaders: [
+            {
+                test: /(\.js|\.jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+            },
+            {
+                test: /\.svg$/,
+                loader: 'url?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]'
+            },
+            {
+                test: /\.woff$/,
+                loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]'
+            },
+            {
+                test: /\.woff2$/,
+                loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]'
+            },
+            {
+                test: /\.[ot]tf$/,
+                loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]'
+            },
+            {
+                test: /\.eot$/,
+                loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]'
+            }
+        ]
     },
     plugins: [
+        new ExtractTextPlugin('style.css'),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({
             minimize: true,
@@ -25,26 +72,4 @@ module.exports = {
             }
         })
     ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss?$/,
-                loader: 'style!css!sass',
-                include: path.join(__dirname, 'src', 'css')
-            },
-            {
-                test: /\.png$/,
-                loader: 'file'
-            },
-            {
-                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                loader: 'file'
-            }
-        ]
-    }
 }
